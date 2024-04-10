4f30577c02611aa961342f2d2bd955c3896d54ab
arpeggi-rpg <148833753+arpeggi-rpg@users.noreply.github.com>
2024-04-10 14:31:35 +0100

    Create divide function

--- scripts/main.js
+++ scripts/main.js
@@ -9,3 +9,8 @@ const subtract = function(num1, num2) {
 const multiply = function(num1, num2) {
     return num1 * num2;
 };
+
+const divide = function(num1, num2) {
+    if (num2 == 0) return "nuh uh";
+    return num1 + num2;
+};
