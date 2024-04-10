427ee86f5e80671eefe18fd9bd15109aa3337065
arpeggi-rpg <148833753+arpeggi-rpg@users.noreply.github.com>
2024-04-10 14:39:08 +0100

    Fix divide function
    Function now divides instead of adding

--- scripts/main.js
+++ scripts/main.js
@@ -12,7 +12,7 @@ const multiply = function(num1, num2) {
 
 const divide = function(num1, num2) {
     if (num2 == 0) return "nuh uh";
-    return num1 + num2;
+    return num1 / num2;
 };
 
 const calc = function(num1, op, num2) {
