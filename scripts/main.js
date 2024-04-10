fc64da23e970a81fb26aa9121e72312b7416bc41
arpeggi-rpg <148833753+arpeggi-rpg@users.noreply.github.com>
2024-04-10 14:38:24 +0100

    Add calc function
    Function takes two numbers and an operator and calls the appropriate calculation

--- scripts/main.js
+++ scripts/main.js
@@ -14,3 +14,22 @@ const divide = function(num1, num2) {
     if (num2 == 0) return "nuh uh";
     return num1 + num2;
 };
+
+const calc = function(num1, op, num2) {
+    switch (op) {
+        case '+':
+            add(num1, num2);
+            break;
+        case '-':
+            subtract(num1, num2);
+            break;
+        case '*':
+            multiply(num1, num2);
+            break;
+        case '/':
+            divide(num1, num2);
+            break;
+        default:
+            break;
+    }
+}
