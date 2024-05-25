3e694d8db7dce0f18c20a09a1df507c92a1f726f
arpeggi-rpg <148833753+arpeggi-rpg@users.noreply.github.com>
2024-05-25 20:11:54 +0100

    Simplify calc functions
    Operators are no longer separate functions

--- scripts/main.js
+++ scripts/main.js
@@ -12,33 +12,17 @@ const decimalButton = document.querySelector("#decimalButton");
 const equalsButton = document.querySelector("#equalsButton");
 const display = document.querySelector("#screenContents");
 
-const add = function(num1, num2) {
-    return num1 + num2;
-};
-
-const subtract = function(num1, num2) {
-    return num1 - num2;
-};
-
-const multiply = function(num1, num2) {
-    return num1 * num2;
-};
-
-const divide = function(num1, num2) {
-    if (num2 == 0) return "nuh uh";  // No dividing by 0 you silly goose
-    return num1 / num2;
-};
-
 const calc = function(num1, num2) {
     switch (op) {
         case '+':
-            return add(num1, num2);
+            return num1 + num2;
         case '-':
-            return subtract(num1, num2);
+            return num1 - num2;
         case '*':
-            return multiply(num1, num2);
+            return num1 * num2;
         case '/':
-            return divide(num1, num2);
+            if (num2 == 0) return "nuh uh";
+            return num1 / num2;
         default:
             break;
     }
